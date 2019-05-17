dc3ddc3904f50417906cd4637dedb74064d84cc6
welbert santos <wsantos@brasilcap.com.br>
2019-05-17 10:40:30 -0300

    alteracoes da estrutura do projeto.

--- app-sequelize/app/models/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-
-    const User = sequelize.define('User', {
-        login: DataTypes.STRING,
-        password: DataTypes.STRING,
-    });
-
-    return User;
-}
